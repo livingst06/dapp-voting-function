76df7ecb4cf8346b48cb03ea27b085962efa77c5
livingst06 <tom.mccallaghan@gmail.com>
2021-04-27 00:01:44 +0200

    op

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import React, { useState, useEffect, useRef } from 'react'
-import {ButtonGroup,ToggleButton} from 'react-bootstrap'
+import { ButtonGroup, ToggleButton } from 'react-bootstrap'
 
 import Voting from './contracts/Voting.json'
 import getWeb3 from './getWeb3'
@@ -14,102 +14,88 @@ import AuthorizeAccount from './components/AuthorizeAccount'
 import VoteFor from './components/VoteFor'
 import TheWinnerBox from './components/TheWinnerBox'
 
-function useIsMountedRef(){
-	const isMountedRef = useRef(null);
+function useIsMountedRef() {
+	const isMountedRef = useRef(null)
 	useEffect(() => {
-	  isMountedRef.current = true;
-	  return () => isMountedRef.current = false;
-	});
-	return isMountedRef;
+		isMountedRef.current = true
+		return () => (isMountedRef.current = false)
+	})
+	return isMountedRef
 }
 
 function App() {
-	
-	const [ web3, setWeb3] 			= useState(null)
-	const [ account, setAccount] 	= useState(null)
-	const [ contract, setContract] 	= useState(null)
-	const [ owner, setOwner] 		= useState(null)
-	const [ wfs, setWfs] 			= useState(null)
-	const [ loading, setLoading]	= useState(true)
-	const [ ws, setWs]				= useState(null)
-	
-	const isMountedRef = useIsMountedRef();
+	const [account, setAccount] = useState(null)
+	const [wfs, setWfs] = useState(null)
+	const [loading, setLoading] = useState(true)
+	const [ws, setWs] = useState(null)
+
+	const web3Ref = useRef(null)
+	const ownerRef = useRef(null)
+	const contractRef = useRef(null)
+	const isMountedRef = useIsMountedRef()
 
 	useEffect(() => {
 		init()
-
 		return () => ws && ws.unsubscribe()
-
-	// eslint-disable-next-line react-hooks/exhaustive-deps
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [])
- 
-
 
 	useEffect(() => {
-		 
-		if (!contract) return 
+		if (!contractRef) return
 
-		if (!web3) return 
-		
+		if (!web3Ref.current) return
+
 		//subscribe to event Voted
-		const client = contract.events.WorkflowStatusChange(
+		const client = contractRef.current.events.WorkflowStatusChange(
 			{
-				fromBlock: web3.eth.getBlock('latest').number,
+				fromBlock: web3Ref.current.eth.getBlock('latest').number,
 			},
 			function (error, event) {
 				//console.log('event after voted',event)
 			}
 		)
 
-		client.on('connected',  () => isMountedRef.current && setWs(client))
+		client.on('connected', () => isMountedRef.current && setWs(client))
 		client.on('changed', function (event) {
-				// remove event from local database
+			// remove event from local database
 		})
 		client.on('error', function (error, receipt) {
-				// If the transaction was rejected by the network with a receipt, the second parameter will be the receipt.
-				console.log('on error', receipt) // same results as the optional callback above
+			// If the transaction was rejected by the network with a receipt, the second parameter will be the receipt.
+			console.log('on error', receipt) // same results as the optional callback above
 		})
 
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [loading])
 
-
 	useEffect(() => {
-		 
 		ws && ws.on('data', (event) => handleWfsChange(event))
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [ws])
 
 	const init = async () => {
-				
 		try {
 			// Get network provider and web3 instance.
-			const _web3 = await getWeb3()
+			web3Ref.current = await getWeb3()
 
 			// set the current account
-			const _accounts = await _web3.eth.getAccounts()
+			const _accounts = await web3Ref.current.eth.getAccounts()
 
 			// Get the contract instance.
-			const networkId = await _web3.eth.net.getId()
+			const networkId = await web3Ref.current.eth.net.getId()
 			const deployedNetwork = Voting.networks[networkId]
-			const _contract = new _web3.eth.Contract(
+			contractRef.current = new web3Ref.current.eth.Contract(
 				Voting.abi,
 				deployedNetwork && deployedNetwork.address
 			)
 
-			const _owner = await _contract.methods.owner().call()
-
+			ownerRef.current = await contractRef.current.methods.owner().call()
 
 			// récupérer le statut etat du vote et le mettre dans le state
-			const _wfs = await _contract.methods.getWorkFlowStatus().call()
+			const _wfs = await contractRef.current.methods.getWorkFlowStatus().call()
 
-			isMountedRef.current && setWeb3(_web3)
 			isMountedRef.current && setAccount(_accounts[0])
-			isMountedRef.current && setContract(_contract)
 			isMountedRef.current && setWfs(_wfs)
-			isMountedRef.current && setOwner(_owner)
 			isMountedRef.current && setLoading(false)
-
 		} catch (error) {
 			// Catch any errors for any of the above operations.
 			alert(
@@ -118,11 +104,9 @@ function App() {
 			console.error(error)
 			isMountedRef.current && setLoading(false)
 		}
-
 	}
- 
+
 	const handleWfsChange = (event) => {
-		
 		switch (parseInt(event.returnValues.newStatus)) {
 			case 0:
 				isMountedRef.current && setWfs('RegisteringVoters')
@@ -145,9 +129,8 @@ function App() {
 			default:
 				break
 		}
-
 	}
- 
+
 	const radios = [
 		{
 			name: 'Enregistrement des votants',
@@ -175,7 +158,7 @@ function App() {
 	const handleToggleChange = async (event) => {
 		// Only owner of the contract can change this toggle.
 		// other users can only see toggle position but cannot change it.
-		if (account !== owner) return
+		if (account !== ownerRef.current) return
 
 		const targetting = event.currentTarget.value
 
@@ -198,87 +181,90 @@ function App() {
 
 		switch (status) {
 			case 'RegisteringVoters':
-				await contract.methods
-					.startRegisteringVoters()
-					.send({ from: account })
+				await contractRef.current.methods.startRegisteringVoters().send({ from: account })
 				break
 			case 'ProposalsRegistrationStarted':
-				await contract.methods
+				await contractRef.current.methods
 					.startProposalRegistration()
 					.send({ from: account })
 				break
 			case 'ProposalsRegistrationEnded':
-				await contract.methods
+				await contractRef.current.methods
 					.stopProposalRegistration()
 					.send({ from: account })
 				break
 			case 'VotingSessionStarted':
-				await contract.methods
-					.startVotingSession()
-					.send({ from: account })
+				await contractRef.current.methods.startVotingSession().send({ from: account })
 				break
 			case 'VotingSessionEnded':
-				await contract.methods
-					.stopVotingSession()
-					.send({ from: account })
+				await contractRef.current.methods.stopVotingSession().send({ from: account })
 				break
 			case 'VotesTallied':
 				// Afficher le div qui affiche le gagnant
-				await contract.methods
-					.votesTallied()
-					.send({ from: account })
+				await contractRef.current.methods.votesTallied().send({ from: account })
 				break
 			default:
 				break
 		}
 	}
 
-	if (!web3) {
+	if (!web3Ref.current) {
 		return <div>please connect to metamask</div>
 	}
 
 	if (loading) {
 		return <div>loading...</div>
 	}
- 
+
 	return (
-			<div className="App">
-				<TheNavbar account={account} />
+		<div className="App">
+			<TheNavbar account={account} />
 
-				<ButtonGroup toggle>
-					{radios.map((radio, idx) => (
-						<ToggleButton
-							key={idx}
-							type="radio"
-							variant={wfs === radio.value ? 'success' : 'secondary'}
-							name="radioToggle"
-							value={radio.value}
-							checked={wfs === radio.value}
-							disabled={owner !== account}
-							onChange={handleToggleChange}
-						>
-							{radio.name}
-						</ToggleButton>
-					))}
-				</ButtonGroup>
+			<ButtonGroup toggle>
+				{radios.map((radio, idx) => (
+					<ToggleButton
+						key={idx}
+						type="radio"
+						variant={wfs === radio.value ? 'success' : 'secondary'}
+						name="radioToggle"
+						value={radio.value}
+						checked={wfs === radio.value}
+						disabled={ownerRef.current !== account}
+						onChange={handleToggleChange}
+					>
+						{radio.name}
+					</ToggleButton>
+				))}
+			</ButtonGroup>
 
-				<br></br>
-				<br></br>
-				{owner === account && wfs === 'RegisteringVoters' && <AuthorizedAccounts web3={web3} contract={contract} />}
-				{owner !== account && wfs === 'RegisteringVoters' && <div>waiting for register your proposal --&gt;&gt;&gt;&gt; </div>}
-				<br></br>
-				{wfs !== 'RegisteringVoters' && isMountedRef.current && <ListProposals web3={web3} contract={contract} />}
-				<br></br>
-				{owner === account && wfs === 'RegisteringVoters' && <AuthorizeAccount  contract={contract} account={account} />}
-				<br></br>
-				{wfs === 'ProposalsRegistrationStarted' && <MakeProposal  contract={contract} account={account} />}
-				<br></br>
-				{wfs === 'VotingSessionStarted' && <VoteFor contract={contract} account={account}/>}
-				<br></br>
-				{wfs === 'VotesTallied' && <TheWinnerBox contract={contract} />}
-			</div>
+			<br></br>
+			<br></br>
+			{ownerRef.current === account && wfs === 'RegisteringVoters' && (
+				<AuthorizedAccounts web3={web3Ref.current} contract={contractRef.current} />
+			)}
+			{ownerRef.current !== account && wfs === 'RegisteringVoters' && (
+				<div>waiting for register your proposal --&gt;&gt;&gt;&gt; </div>
+			)}
+			<br></br>
+			{wfs !== 'RegisteringVoters' && isMountedRef.current && (
+				<ListProposals web3={web3Ref.current} contract={contractRef.current} />
+			)}
+			<br></br>
+			{ownerRef.current === account && wfs === 'RegisteringVoters' && (
+				<AuthorizeAccount contract={contractRef.current} account={account} />
+			)}
+			<br></br>
+			{wfs === 'ProposalsRegistrationStarted' && (
+				<MakeProposal contract={contractRef.current} account={account} />
+			)}
+			<br></br>
+			{wfs === 'VotingSessionStarted' && (
+				<VoteFor contract={contractRef.current} account={account} />
+			)}
+			<br></br>
+			{wfs === 'VotesTallied' && <TheWinnerBox contract={contractRef.current} />}
+		</div>
 	)
-	
 }
 
 export default App
